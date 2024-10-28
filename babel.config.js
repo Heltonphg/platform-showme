0045040df77c4b2141e9861c58c367704fd723d3
Helton Wylammi <wylammi.helton@gmail.com>
2024-10-28 18:52:09 -0300

    chore: module resolver config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@dtos': './src/dtos',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@storage': './src/storage',
+            '@utils': './src/utils',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@contexts': './src/contexts',
+            '@routes': './src/routes',
+            '@libs': './src/libs',
+            '@styles': './src/styles',
+            '@theme': './src/theme',
+            '@type': './src/types',
+            '@__tests__': './__tests__'
+          }
+        }
+      ]
+    ]
+  }
+}
